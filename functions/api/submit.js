f5578c2e04a1d45e66ab86248c19f791275bfe15
joesb <joesb@users.noreply.github.com>
2024-12-02 17:11:04 +0000

    deploy: 18243ebf1e5b16ed349cba122a1a63645ff24878

--- functions/api/submit.js
+++ functions/api/submit.js
@@ -1,10 +1,11 @@
 /**
  * POST /api/submit
  */
+import { Resend } from 'resend';
+
 export async function onRequestPost(context) {
   try {
     let input = await context.request.formData();
-
     // Convert FormData to JSON
     // NOTE: Allows multiple values per key
     let output = {};
@@ -16,14 +17,34 @@ export async function onRequestPost(context) {
         output[key] = [].concat(tmp, value);
       }
     }
-
-    let pretty = JSON.stringify(output, null, 2);
-    return new Response(pretty, {
-      headers: {
-        "Content-Type": "application/json;charset=utf-8",
-      },
+    // output: {
+    //   name: 'Jane Doe',
+    //   'contact-name': '',
+    //   email: 'jane@doe.com',
+    //   subject: 'help me',
+    //   message: 'this is my message'
+    const honeypot = output["contact-name"]
+    // Return early with pretend confirmation if bot hit honeypot
+    if (honeypot !== "") {
+      return Response.redirect("https://helios360.co.uk/contact-confirmation", 303)
+    }
+    // Using text instead of email so that I don't need to sanitize it
+    const resend = new Resend(context.env.RESEND_API_KEY);
+    const { data, error } = await resend.emails.send({
+      from: context.env.SENDER_EMAIL,
+      reply_to: output.email,
+      to: context.env.RECIPIENT_EMAIL,
+      subject: `[Helios360] Contact form request from ${output.fullname}: ${output.subject}`,
+      text: output.message,
     });
+    console.log({data, error});
+    if (error) {
+      return Response.redirect("https://helios360.co.uk/404", 303)
+    } else {
+      return Response.redirect("https://helios360.co.uk/contact-confirmation", 303)
+    }
   } catch (err) {
-    return new Response("Error parsing JSON content", { status: 400 });
+    console.log(err);
+    return Response.redirect("https://helios360.co.uk/404?error=json_parsing", 303)
   }
 }
